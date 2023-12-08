431b5265349c0ff16359d56ece5fb29748ca8246
igorlanko <mail@igorlanko.com>
2023-12-08 16:15:44 -0500

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by stranger on 2023-12-08.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Rest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Better Rest
+//
+//  Created by stranger on 2023-12-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
