88f787e335132ea5f21cc35288b4af73d10ab903
igorlanko <mail@igorlanko.com>
2023-12-08 16:58:15 -0500

    examples of date ranges

--- Better Rest/ContentView.swift
+++ Better Rest/ContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
 	@State private var sleepAmount = 8.0
+	@State private var wakeUp = Date.now
 	
     var body: some View {
+		DatePicker("Enter a date", selection: $wakeUp, in: Date.now...)
+			.labelsHidden()
 		Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
     }
+	
+	func exampleDates() {
+		let tomorrow = Date.now.addingTimeInterval(86400)
+		let range = Date.now...tomorrow
+	}
 }
 
 #Preview {
